60a1249448c77e41c5c6a343174d899775585357
Anton Kuznetsov <isnifer@gmail.com>
2015-10-08 17:43:05 +0300

    Gitignore fix

---
+++ defaultValidator/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Valya from 'valya';
+
+@Valya
+export default class Validator extends React.Component {
+    static displayName = 'Validator';
+
+    _renderError() {
+        if (!this.props.enabled || this.props.isValid) {
+            return null;
+        }
+
+        return (
+            <span className="validator__error" key="error">
+                {this.props.validationErrorMessage}
+            </span>
+        );
+    }
+
+    render () {
+
+        return (
+            <span className="validator">
+                <span className="validator__target" key="target">
+                    {this.props.children}
+                </span>
+                {this._renderError()}
+            </span>
+        );
+    }
+}
